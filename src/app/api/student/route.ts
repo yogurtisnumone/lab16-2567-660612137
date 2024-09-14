4eac21803eae0a6e296a0664aeb42d3fdc4195dc
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-09-14 17:42:24 +0700

    6!

--- src/app/api/student/route.ts
+++ src/app/api/student/route.ts
@@ -114,6 +114,13 @@ export const DELETE = async (request: NextRequest) => {
     );
   }
 
+  if (body.studentId.length !== 9) {
+    return NextResponse.json(
+      { ok: false, message: "Student Id must contain 9 characters" },
+      { status: 400 }
+    );
+  }
+
   //check if student id exist
   const foundIndex = DB.students.findIndex(
     (std) => std.studentId === body.studentId
@@ -122,13 +129,6 @@ export const DELETE = async (request: NextRequest) => {
     return NextResponse.json(
       { ok: false, message: "Student ID does not exist" },
       { status: 404 }
-    );
-  }
-
-  if (body.studentId.length !== 9) {
-    return NextResponse.json(
-      { ok: false, message: "Student Id must contain 9 characters" },
-      { status: 400 }
     );
   }
 
